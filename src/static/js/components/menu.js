6d9f84acb79054a6da1461414ab33cde35f816fa
Ihor <icassidyif@gmail.com>
2021-10-24 03:46:36 +0300

    change main menu and header

--- src/static/js/components/menu.js
+++ src/static/js/components/menu.js
@@ -1,17 +1,26 @@
 const burger = document.querySelector('.burger');
 const menu = document.querySelector('.menu');
-const menuItems = document.querySelectorAll('.menu__item');
+const body = document.querySelector('body');
+const menuArrows = document.querySelectorAll('.menu__arrow');
 
+if (menu) {
 
-menuItems.forEach(item => {
-  let style = getComputedStyle(item);
-  let marginRight = parseInt(style.marginRight);
-  item.style.width = item.clientWidth + marginRight + 'px';
-  item.style.height = item.clientHeight + 'px';
-})
+  menuArrows.forEach(arrow => {
+    arrow.addEventListener('click', (e) => {
+      const currentSubMenu = arrow.nextElementSibling;
+      currentSubMenu.classList.toggle('show');
+      const arrowSvg = arrow.querySelector('svg');
+      if (currentSubMenu.classList.contains('show')) {
+        arrowSvg.style.transform = 'rotate(-180deg)';
+      } else arrowSvg.style.transform = 'rotate(180deg)';
+    })
+  })
 
-burger.addEventListener('click', e => {
-  menu.classList.toggle('active');
-  burger.classList.toggle('active');
-})
+  burger.addEventListener('click', e => {
+    menu.classList.toggle('active');
+    burger.classList.toggle('active');
+    body.classList.toggle('lock');
+  })
+}
+
 
